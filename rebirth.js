4dd5e2eeb3b3fba480680cf7479508889da40687
silverwind <me@silverwind.io>
2015-08-07 23:26:43 +0200

    Update all the things, reindent

--- rebirth.js
+++ rebirth.js
@@ -4,13 +4,13 @@ var extend = require("util-extend");
 var spawn  = require("child_process").spawn;
 
 module.exports = function rebirth(opts) {
-    opts = extend({
-        cwd      : process.cwd,
-        detached : true,
-        env      : process.env,
-        stdio    : "inherit"
-    }, opts);
+  opts = extend({
+    cwd      : process.cwd,
+    detached : true,
+    env      : process.env,
+    stdio    : "inherit"
+  }, opts);
 
-    spawn(process.execPath, process.argv.slice(1), opts).unref();
-    process.exit(0);
+  spawn(process.execPath, process.argv.slice(1), opts).unref();
+  process.exit(0);
 };
--- test.js
+++ test.js
@@ -2,19 +2,19 @@
 
 process.title = "phoenix";
 console.log([
-    "  _/|       |\\_",
-    " /  |       |  \\",
-    "|    \\     /    |",
-    "|  \\ /     \\ /  |",
-    "| \\  |     |  / |",
-    "| \\ _\\_/^\\_/_ / |  I am (re)born with PID " + process.pid,
-    "|    --\\//--    |",
-    " \\_  \\     /  _/   Respawning in 10 seconds. Kill me with 'killall phoenix'",
-    "   \\__  |  __/",
-    "      \\ _ /",
-    "     _/   \\_",
-    "    / _/|\\_ \\",
-    "     /  |  \\",
-    "      / v \\",
+  "  _/|       |\\_",
+  " /  |       |  \\",
+  "|    \\     /    |",
+  "|  \\ /     \\ /  |",
+  "| \\  |     |  / |",
+  "| \\ _\\_/^\\_/_ / |  I am (re)born with PID " + process.pid,
+  "|    --\\//--    |",
+  " \\_  \\     /  _/   Respawning in 10 seconds. Kill me with 'killall phoenix'",
+  "   \\__  |  __/",
+  "      \\ _ /",
+  "     _/   \\_",
+  "    / _/|\\_ \\",
+  "     /  |  \\",
+  "      / v \\",
 ].join("\n"));
 setTimeout(require("./"), 10000);
