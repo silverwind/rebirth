91b92d8aab4eb3d9ba6b6fb2c0622abf70e993f0
silverwind <me@silverwind.io>
2017-07-30 09:22:15 +0200

    refactor, rename to index.js

--- index.js
+++ index.js
@@ -3,13 +3,11 @@
 const spawn  = require("child_process").spawn;
 
 module.exports = function rebirth(opts) {
-  opts = Object.assign({
+  spawn(process.execPath, process.argv.slice(1), Object.assign({
     cwd      : process.cwd(),
     detached : true,
     env      : Object.assign(process.env, {REBORN: 1}),
     stdio    : "inherit"
-  }, opts);
-
-  spawn(process.execPath, process.argv.slice(1), opts).unref();
+  }, opts)).unref();
   process.exit(0);
 };
